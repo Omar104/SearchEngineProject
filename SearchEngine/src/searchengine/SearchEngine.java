b0357c31c959c09afba476ccd5fb0558a639b3eb
osaidlabib <osaidlabib@gmail.com>
2017-03-24 00:28:20 +0200

    Add files via upload

--- SearchEngine/src/searchengine/SearchEngine.java
+++ SearchEngine/src/searchengine/SearchEngine.java
@@ -12,6 +12,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,24 +28,37 @@ public class SearchEngine {
     public static void main(String[] args) {
        
         DataBase DB2 = new DataBase("root","");
+        
         DB2.deleteAll("basecount");
         DB2.deleteAll("crawlerqueue");
-        DB2.deleteAll("crawlerset");
         int maxCrawler=DB2.getMaxCrawler();
-        WebCrawler seed1=new WebCrawler("https://en.wikipedia.org/",50,maxCrawler+1);
-        WebCrawler seed2=new WebCrawler("https://google.com.eg/",50,maxCrawler+1);
-   
-        try{
-            seed1.join();
-            seed2.join();
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Enter number of threads");
+        int NumberOfThreads=scanner.nextInt();
+        int TotalSize=5000;
+        String URLSeeds[]={"https://en.wikipedia.org/","https://www.google.com.eg/","https://www.youtube.com","https://www.yahoo.com/"};
+        WebCrawler[] Seeds =new WebCrawler[6];
+        
+        for(int i=0;i<NumberOfThreads;i++){
+            Seeds[i]=(new WebCrawler(URLSeeds[i%URLSeeds.length],TotalSize,maxCrawler+1));
+            
         }
+        
+       
+        try{
+            for (WebCrawler Seed : Seeds) {
+                Seed.join();
+            }
+        }
+   
+      
         catch (InterruptedException e)
         {
             System.out.println("seed1  interputed"); 
         }
-         System.out.println("Size:");
+       
         
-        System.out.println(DB2.getSize("crawlerqueue"));
+        System.out.println("get "+DB2.getSize("crawlerset",maxCrawler+1));
        
     }
     
